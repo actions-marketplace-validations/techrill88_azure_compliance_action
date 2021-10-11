d3ad2c2a39234f450150bcc9bd33c4d72c983395
Ambily <ambily.k@tcs.com>
2021-10-11 15:47:30 +0530

    inspec modifications

--- app/index.sh
+++ app/index.sh
@@ -1,10 +1,10 @@
 echo "Goodbye"
-curl https://omnitruck.chef.io/install.sh | sudo bash -s -- -P inspec --chef-license accept-silent
+curl https://omnitruck.chef.io/install.sh | sudo bash -s -- -P inspec 
 export AZURE_SUBSCRIPTION_ID= $Azure_subscriptionId
 export AZURE_CLIENT_ID=$Azure_clientId
 export AZURE_CLIENT_SECRET=$Azure_clientSecret
 export AZURE_TENANT_ID=$Azure_tenantId
-inspec exec . -t azure:// --reporter cli junit:testresults.xml html:report.html
-inspec exec inspec.yml -t azure:// --reporter cli junit:testresults.xml html:report.html
+inspec exec . -t azure:// --reporter cli junit:testresults.xml html:report.html --chef-license accept-silent
+inspec exec inspec.yml -t azure:// --reporter cli junit:testresults.xml html:report.html --chef-license accept-silent
 
 #REM inspec exec inspec.yml -t azure:// https://github.com/dev-sec/ssl-baseline --reporter cli junit:testresults.xml html:report.html
