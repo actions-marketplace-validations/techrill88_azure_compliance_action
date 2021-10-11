9fa90deb2fdb5e1f166251eae1a52b8430cd39c6
Ambily <ambily.k@tcs.com>
2021-10-11 17:38:26 +0530

    inspec modifications

--- app/inspec-azure-check-profile/controls/test.rb
+++ app/inspec-azure-check-profile/controls/test.rb
@@ -1,4 +1,14 @@
-include_controls "inspec-azure"
-include_controls "linux-baseline"
-include_controls "ssh-baseline"
-include_controls "windows-baseline"
+# copyright: 2021, The Authors
+
+title "Azure Compliance Checker"
+
+# you add controls here
+control "azure-virtual-machines-exist-check" do                                # A unique ID for this control.
+  impact 1.0                                                                   # The criticality, if this control fails.
+  title "Check resource groups to see if any VMs exist."                       # A human-readable title
+  azurerm_resource_groups.names.each do |resource_group_name|                  # Plural resources can be leveraged to loop across many resources
+    describe azurerm_virtual_machines(resource_group: resource_group_name) do
+      it { should exist } # The test itself.
+    end
+  end
+end
