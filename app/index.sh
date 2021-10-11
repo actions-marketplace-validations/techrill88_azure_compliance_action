585a2963eb33d97466efde82a1fbf66f13c0097c
Ambily <ambily.k@tcs.com>
2021-10-11 17:51:52 +0530

    inspec modifications

--- app/index.sh
+++ app/index.sh
@@ -3,10 +3,10 @@ echo "Goodbye  : $1 "
 curl https://omnitruck.chef.io/install.sh | sudo bash -s -- -P inspec 
 cp -r $1/app/inspec-azure-check-profile .
 
-export subscription_id= $Azure_subscriptionId
-export client_id=$Azure_clientId
-export client_secret=$Azure_clientSecret
-export tenant_id=$Azure_tenantId
+export :subscription_id= $Azure_subscriptionId
+export :client_id=$Azure_clientId
+export :client_secret=$Azure_clientSecret
+export :tenant_id=$Azure_tenantId
 
 #REM inspec exec . -t azure:// --reporter cli junit:testresults.xml html:report.html --chef-license accept-silent
 inspec check inspec-azure-check-profile --chef-license accept-silent
