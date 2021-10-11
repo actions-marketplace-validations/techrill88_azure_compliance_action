65a1974c54278d1db0e3cb7844590945c786ef29
Ambily <ambily.k@tcs.com>
2021-10-11 15:56:25 +0530

    inspec modifications

--- app/index.sh
+++ app/index.sh
@@ -5,6 +5,6 @@ export AZURE_CLIENT_ID=$Azure_clientId
 export AZURE_CLIENT_SECRET=$Azure_clientSecret
 export AZURE_TENANT_ID=$Azure_tenantId
 inspec exec . -t azure:// --reporter cli junit:testresults.xml html:report.html --chef-license accept-silent
-inspec exec inspec.yml -t azure:// --reporter cli junit:testresults.xml html:report.html --chef-license accept-silent
+inspec exec ./inspec.yml -t azure:// --reporter cli junit:testresults.xml html:report.html --chef-license accept-silent
 
 #REM inspec exec inspec.yml -t azure:// https://github.com/dev-sec/ssl-baseline --reporter cli junit:testresults.xml html:report.html
