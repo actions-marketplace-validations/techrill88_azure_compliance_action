c33b43406d88efa300a5f6900d52e0052b92ed39
Ambily <ambily.k@tcs.com>
2021-10-12 15:20:33 +0530

    inspec modifications

--- app/inspec-azure-check-profile/controls/resourcegroup.rb
+++ app/inspec-azure-check-profile/controls/resourcegroup.rb
@@ -3,3 +3,9 @@ control 'azure_resource_groups' do
         its('names')  { should include 'rg-core' }
     end
 end
+
+control 'azure-rg-exists' do
+    describe azure_resource_groups do
+        it { should exist }
+    end
+emd
