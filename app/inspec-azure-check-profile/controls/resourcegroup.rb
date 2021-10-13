dd32925b0c0421fa86afa41240bb1ed649f01924
Ambily <ambily.k@tcs.com>
2021-10-13 20:45:32 +0530

    inspec modifications

--- app/inspec-azure-check-profile/controls/resourcegroup.rb
+++ app/inspec-azure-check-profile/controls/resourcegroup.rb
@@ -1,11 +1,15 @@
-control 'azure_resource_groups' do
-    describe azure_resource_groups do
-        its('names')  { should include 'rg-core' }
-    end
-end
-
 control 'azure-rg-exists' do
     describe azure_resource_groups do
         it { should exist }
     end
 end
+
+control "azure-virtual-machines-exist-check" do                                # A unique ID for this control.   
+    title "Check resource groups to see if any VMs exist."                       # A human-readable title
+    azure_resource_groups.names.each do |resource_group_name|                  # Plural resources can be leveraged to loop across many resources
+      describe azurerm_virtual_machines(resource_group: resource_group_name) do
+        it { should exist } # The test itself.
+      end
+    end
+  end
+  
