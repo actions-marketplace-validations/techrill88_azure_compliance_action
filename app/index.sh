89f0581f092dfdcb3ab119ab459a0c67a30f7560
Ambily <ambily.k@tcs.com>
2021-10-16 21:51:26 +0530

    inspec modifications

--- app/index.sh
+++ app/index.sh
@@ -6,6 +6,7 @@ export AZURE_CLIENT_ID=$3
 export AZURE_TENANT_ID=$4
 export AZURE_CLIENT_SECRET=$5
 
+test_file_name = $6
 
 curl https://omnitruck.chef.io/install.sh | sudo bash -s -- -P inspec 
 cp -r $1/app/inspec-azure-check-profile .
@@ -15,6 +16,7 @@ cp -r $1/app/inspec-azure-check-profile .
 #REM inspec check inspec-azure-check-profile --chef-license accept-silent
 inspec exec inspec-azure-check-profile/ -t azure:// --chef-license accept-silent
 
-#REM inspec exec inspec.yml -t azure:// https://github.com/dev-sec/ssl-baseline --reporter cli junit:testresults.xml html:report.html
-
-#REM :tenant_id, :client_id, :client_secret, :subscription_id
+if [$test_file_name]
+then
+    inspec exec $test_file_name -t azure:// --chef-license accept-silent
+fi
