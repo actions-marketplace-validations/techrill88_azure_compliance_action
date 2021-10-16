4a0ba4331bbff6e2d003f193032202e884fc4d1c
Ambily <ambily.k@tcs.com>
2021-10-16 22:10:20 +0530

    inspec modifications

--- app/index.sh
+++ app/index.sh
@@ -6,8 +6,6 @@ export AZURE_CLIENT_ID=$3
 export AZURE_TENANT_ID=$4
 export AZURE_CLIENT_SECRET=$5
 
-test_file_name = $6
-
 curl https://omnitruck.chef.io/install.sh | sudo bash -s -- -P inspec 
 cp -r $1/app/inspec-azure-check-profile .
 
@@ -16,7 +14,10 @@ cp -r $1/app/inspec-azure-check-profile .
 #REM inspec check inspec-azure-check-profile --chef-license accept-silent
 inspec exec inspec-azure-check-profile/ -t azure:// --chef-license accept-silent
 
-if [ ! -z "$test_file_name" ]
+echo "test file"
+echo $6
+
+if [ ! -z "$6" ]
 then
-    inspec exec $test_file_name -t azure:// --chef-license accept-silent
+    inspec exec $6 -t azure:// --chef-license accept-silent
 fi
