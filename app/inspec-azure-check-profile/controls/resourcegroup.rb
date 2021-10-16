1612edb62a9f68ef19b6e70f056fd25aaa3dd87d
Ambily <ambily.k@tcs.com>
2021-10-16 20:43:21 +0530

    inspec modifications

--- app/inspec-azure-check-profile/controls/resourcegroup.rb
+++ app/inspec-azure-check-profile/controls/resourcegroup.rb
@@ -1,17 +1,17 @@
-control 'azure-rg-exists' do
-    describe azure_resource_groups do
-        it { should exist }
-    end
-end
 
 control "azure-virtual-machines-exist-check" do                                # A unique ID for this control.   
-    title "Check resource groups to see if any VMs exist."                       # A human-readable title
-    azure_resource_groups.names.each do |resource_group_name|                  # Plural resources can be leveraged to loop across many resources
+    title "Check resource groups to see if any VMs exist."                      
+    azure_resource_groups.names.each do |resource_group_name|   
+      put "RG name #{resource_group_name}"            
       describe azurerm_virtual_machines(resource_group: resource_group_name) do
         it { should exist } # The test itself.
       end
     end
-    
+
+    describe azure_resource_groups do
+      it { should exist }
+    end
+
     describe azure_virtual_machine(resource_group: 'rg-core', name: 'workvm') do
         it { should have_only_approved_extensions(['ApprovedExtension', 'OtherApprovedExtensions']) }
     end
