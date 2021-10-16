b662b2ee41ea1075dfe5054789aa2655f2133c0c
Ambily <ambily.k@tcs.com>
2021-10-16 21:42:55 +0530

    inspec modifications

--- app/inspec-azure-check-profile/controls/webapp.rb
+++ app/inspec-azure-check-profile/controls/webapp.rb
@@ -1,10 +1,19 @@
-# you add controls here
-control "azure-webapp-check" do                                # A unique ID for this control.
+# Basic compliance check for Web apps
+control "azure-webapp-check" do                                
   impact 1.0                                                                   
-  title "Check resource groups to see if any VMs exist."                       
+  title "Check the properties of Web apps"                       
+  desc "Web app authentication and validation rules set or not "
   azure_resource_groups.names.each do |resource_group_name|                  
-    describe azurerm_virtual_machines(resource_group: resource_group_name) do
-      it { should exist } 
+    azure_webapps.names.each do |webapp_name| 
+      describe azure_webapp(resource_group: resource_group_name, name: webapp_name) do
+        it { should exist }     
+        # true if the Authentication / Authorization feature is enabled for the current app; otherwise, false.   
+        its('auth_settings.properties') { should have_attributes(enabled: true ) }
+        # Gets a value indicating whether the issuer should be a valid HTTPS url and be validated as such.
+        its('auth_settings.properties') { should have_attributes(validateIssuer: true ) }
+        # Virtual Network Route All enabled. This causes all outbound traffic to have Virtual Network Security Groups and User Defined Routes applied.
+        its('configuration.properties') { should have_attributes(vnetRouteAllEnabled: true) }
+      end
     end
   end
 end
