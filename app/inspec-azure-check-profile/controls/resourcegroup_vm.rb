a29bbf23e924309989333acfb318d459f579260d
Ambily <ambily.k@tcs.com>
2021-10-17 12:30:44 +0530

    compliance check separated

--- app/inspec-azure-check-profile/controls/resourcegroup_vm.rb
+++ app/inspec-azure-check-profile/controls/resourcegroup_vm.rb
@@ -1,13 +1,8 @@
 
-control "azure-virtual-machines-check" do                                # A unique ID for this control.   
+control "azure-vm-sample" do                                # A unique ID for this control.   
    
     describe azure_resource_groups do
       it { should exist }
     end
-
-    describe azure_virtual_machine(resource_group: 'rg-main', name: 'testmachine') do
-        it { should exist }  # have_only_approved_extensions(['ApprovedExtension', 'OtherApprovedExtensions']) }
-    end
-    
   end
   
