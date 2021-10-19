6dfd4e9e05403f90656921b6ca2d5edda9760914
Ambily <ambily.k@tcs.com>
2021-10-19 14:01:31 +0530

    realigning the action code

--- app/index.sh
+++ app/index.sh
@@ -1,21 +1,24 @@
 
-echo "Start Execution"
+echo "azure_compliance-action execution started"
 
+echo $INPUT_COMPLIANCE_TEST_PROFILE_URL
+# export the values as environment variables
 export AZURE_SUBSCRIPTION_ID=$2
 export AZURE_CLIENT_ID=$3
 export AZURE_TENANT_ID=$4
 export AZURE_CLIENT_SECRET=$5
 
+# install the Chef Inspec
 curl https://omnitruck.chef.io/install.sh | sudo bash -s -- -P inspec 
-cp -r $1/app/inspec-azure-check-profile .
 
-
-inspec exec inspec-azure-check-profile/ -t azure:// --chef-license accept-silent
-
-echo "test file"
-echo $6
-
+# execute the compliance tests from the profile, if available. 
+# Otherwise, execute the sample available as part of the action
 if [ ! -z "$6" ]
 then    
     inspec exec $6 -t azure:// --chef-license accept-silent
+else
+    cp -r $1/app/inspec-azure-check-profile .
+    inspec exec inspec-azure-check-profile/ -t azure:// --chef-license accept-silent
 fi
+
+echo "azure_compliance-action execution completed"
--- app/inspec-azure-check-profile/controls/resourcegroup_sample.rb
+++ app/inspec-azure-check-profile/controls/resourcegroup_sample.rb
@@ -1,6 +1,8 @@
 
-control "azure-rg-exists-sample" do                                # A unique ID for this control.   
-   
+# sample compliance test to verify the resource group exists or not
+control "azure-rg-exists-sample" do   
+    impact 0.1
+    desc "sample test to verify any resource group exists in the subscription"    
     describe azure_resource_groups do
       it { should exist }
     end
